9444ded72149f6be515782726053b50fbb1fc107
Occamssrazor <natalie-0@mail.ru>
2024-12-09 00:42:44 +0300

    Recent

--- db_service/db_connect.py
+++ db_service/db_connect.py
@@ -2,14 +2,14 @@ import psycopg2
 
 dbname = 'postgres'
 user = 'postgres'
-password = 'docker'
-host = 'localhost'
+password = 'qweasdzxc123'
+host = 'db_service'
 port = '5432'
 sql_statement = '''CREATE TABLE USERS
 (
     Id SERIAL PRIMARY KEY,
-    Email TEXT
-    FirstName TEXT
+    Email TEXT,
+    FirstName TEXT,
     LastName TEXT
 );'''
 conn = psycopg2.connect(
@@ -20,3 +20,6 @@ print(conn)
 cursor = conn.cursor()
 cursor.execute(sql_statement)
 conn.commit()
+cursor.close()
+conn.close()
+print("Table created successfully")
